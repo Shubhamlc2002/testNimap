e378ae903ed9d2b315f3ad66e0f15a0ee48d7c75
Shubhamlc2002 <shubhamlchavan2002@gmail.com>
2024-10-08 21:32:00 +0530

    Update ProductRepository.java

--- crud/src/main/java/com/example/crud/repository/ProductRepository.java
+++ crud/src/main/java/com/example/crud/repository/ProductRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.crud.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    // Additional query methods if needed
+    
 }
